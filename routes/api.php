978ad267bde14ca2951e5cc28c9cfde98e21a25f
Caio Reis <caiofreis2005@gmail.com>
2024-09-02 23:29:31 -0300

    API routes updated.

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\ManagerController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/manager', 'App\Http\Controllers\Api\ManagerController@index');
-Route::get('/admin', 'App\Http\Controllers\Api\AdminController@index');
+Route::get('/manager', [ManagerController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index']);
