67f37377706efb64876034a94ef72a8c00f9c539
Caio Reis <caiofreis2005@gmail.com>
2024-09-04 11:59:58 -0300

    adding helpers.

---
+++ app/Helpers/generateTransferLimit.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\Account;
+
+if(!function_exists('generateTransferLimit')){
+    function generateTransferLimit(){
+        $faker = \Faker\Factory::create();
+
+        $randomLimit = $faker->randomFloat(2, 1000, 3500);
+
+        return $randomLimit;
+    }
+}
---
+++ app/Helpers/helpers.php
@@ -0,0 +1,4 @@
+<?php
+
+include __DIR__ . '/generateUnicAccountNumber.php';
+include __DIR__ . '/generateTransferLimit.php';
