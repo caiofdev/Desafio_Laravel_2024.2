6faa4e03e579849a73b36ba1de37ea7da5ca2b9c
Caio Reis <caiofreis2005@gmail.com>
2024-12-09 19:26:39 -0300

    fixing modal bug

--- resources/views/modals/update.blade.php
+++ resources/views/modals/update.blade.php
@@ -85,11 +85,11 @@
                         <label class="block mb-1 text-sm font-medium text-white">Profile Photo</label>
                         <input type="file" name="photo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" />
                     </div>
-                    
-                    <div class="mt-8 text-center">
-                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
-                        <button type="button" onclick="closeModal('updateModal-{{ $admin->id }}')" class="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Close</button>
-                    </div>
+                </div>
+
+                <div class="mt-8 text-center flex justify-center">
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+                    <button type="button" onclick="closeModal('updateModal-{{ $admin->id }}')" class="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ml-1">Close</button>
                 </div>
             </form>
         </div>
